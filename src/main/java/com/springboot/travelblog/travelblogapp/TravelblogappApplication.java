14edee9bc0cb5cbe1e356336d38d0bec53c42f6f
unknown <nvyas3095@gmail.com>
2023-07-02 18:50:24 -0700

    Added all files from new project.

---
+++ src/main/java/com/springboot/travelblog/travelblogapp/TravelblogappApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.travelblog.travelblogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelblogappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelblogappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/travelblog/travelblogapp/TravelblogappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.travelblog.travelblogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelblogappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
