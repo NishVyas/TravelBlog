cc545cb24e158c450846764174b6a0e162d90baf
Nishant Vyas <nvyas3095@gmail.com>
2023-07-04 22:53:33 -0700

    Added custom application properties to use. Created a bloginfo endpoint to display these custom properties.

--- src/main/java/com/springboot/travelblog/travelblogapp/rest/FunRestController.java
+++ src/main/java/com/springboot/travelblog/travelblogapp/rest/FunRestController.java
@@ -1,13 +1,24 @@
 package com.springboot.travelblog.travelblogapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
-    // Expose a "/" endpoint that returns "Hello World"
+    @Value("${traveller.name}")
+    private String travellerName;
 
+    @Value("${location.name}")
+    private String locationName;
+
+    @GetMapping("/bloginfo")
+    public String getBlogInfo() {
+        return "Traveller: " + travellerName + ", Location: " + locationName;
+    }
+
+    // Expose a "/" endpoint that returns "Hello World"
     @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
