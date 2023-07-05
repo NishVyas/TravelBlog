211915113fe807285872617a083e4f4e565becc3
Nishant Vyas <nvyas3095@gmail.com>
2023-07-04 22:14:01 -0700

    Added a couple of new endpoints. Also added spring boot dev tools in order for my project to reload anytime i make changes to my work.

--- src/main/java/com/springboot/travelblog/travelblogapp/rest/FunRestController.java
+++ src/main/java/com/springboot/travelblog/travelblogapp/rest/FunRestController.java
@@ -13,4 +13,14 @@ public class FunRestController {
         return "Hello World!";
     }
 
+    // Expose a ne endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day.";
+    }
 }
