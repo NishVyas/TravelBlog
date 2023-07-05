e1ac907c87418124d2d370a8cee28854e8222a12
Nishant Vyas <nvyas3095@gmail.com>
2023-07-04 23:01:55 -0700

    Changed the port number. Also changed the apllication context path. All requests now have to be prefixed with travelblog.

--- src/main/java/com/springboot/travelblog/travelblogapp/rest/FunRestController.java
+++ src/main/java/com/springboot/travelblog/travelblogapp/rest/FunRestController.java
@@ -18,13 +18,11 @@ public class FunRestController {
         return "Traveller: " + travellerName + ", Location: " + locationName;
     }
 
-    // Expose a "/" endpoint that returns "Hello World"
     @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
     }
 
-    // Expose a ne endpoint for "workout"
     @GetMapping("/workout")
     public String getDailyWorkout() {
         return "Run a hard 5k";
