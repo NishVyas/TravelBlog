eed7dad3d268de6e48bf51d29e9ba2c1a02860cb
Nishant Vyas <nvyas3095@gmail.com>
2023-07-16 11:42:23 -0700

    Added a createTravelBlogger() method in the main class. Using the injected dependency of the TravelBloggerDAO, I first create the TravelBlogger object, populate it with fields, then use the injected dao to save the created object. I also display the generated ID of the saved TravelBlogger. This method gets used by the command line runner bean.

--- src/main/java/com/springboot/travelblog/travelblogapp/TravelblogappApplication.java
+++ src/main/java/com/springboot/travelblog/travelblogapp/TravelblogappApplication.java
@@ -1,5 +1,7 @@
 package com.springboot.travelblog.travelblogapp;
 
+import com.springboot.travelblog.travelblogapp.dao.TravelBloggerDAO;
+import com.springboot.travelblog.travelblogapp.entity.TravelBlogger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,14 +10,26 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class TravelblogappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TravelblogappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TravelblogappApplication.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
-		return runner -> {
-			System.out.println("Hello World");
-		};
-	}
+    @Bean
+    public CommandLineRunner commandLineRunner(TravelBloggerDAO travelBloggerDAO) {
+        return runner -> {
+            createTravelBlogger(travelBloggerDAO);
+        };
+    }
+
+    private void createTravelBlogger(TravelBloggerDAO travelBloggerDAO) {
+        System.out.println("Creating a new Travel Blogger...");
+        TravelBlogger travelBlogger = new TravelBlogger("Nishant", "Vyas", "nishant.vyas@gmail.com");
+
+        System.out.println("Saving the Travel Blogger...");
+        travelBloggerDAO.save(travelBlogger);
+
+        System.out.println("Saved Travel Blogger. Generated ID: " + travelBlogger.getId());
+    }
+
+
 }
