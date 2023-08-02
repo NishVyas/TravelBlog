d441dd40a2bcf30d1785ac0e7bcdaea71bdf4bd2
unknown <nvyas3095@gmail.com>
2023-08-02 03:14:24 -0700

    Added the @RepositoryRestResource annotation to account for plural exceptions like Goose. This allows me to customize my exposed endpoint name linked to my Entity type. Also added application property for default page size when sending a request to those endpoints. Also added Postman requests for sorting the GET requests by title ascending and descending.

--- src/main/java/com/springboot/travelblog/travelblogapp/dao/BlogRepository.java
+++ src/main/java/com/springboot/travelblog/travelblogapp/dao/BlogRepository.java
@@ -2,6 +2,8 @@ package com.springboot.travelblog.travelblogapp.dao;
 
 import com.springboot.travelblog.travelblogapp.entity.Blog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="weblogs")
 public interface BlogRepository extends JpaRepository<Blog, Integer> {
 }
