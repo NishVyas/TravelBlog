23e814621aac1f9d65f44a67a6985769bc554c64
unknown <nvyas3095@gmail.com>
2023-08-15 15:24:58 -0700

    Added actual Users and Authorities within my own database instead of having hard-coded users in-memory. Commented out code for creating users in-memory within SecurityConfig.java. Added code to register an instance of UserDetailsManager with a parameter injected DataSource, which also gets auto-configured by Spring using the @Bean annotation. This new code will simply configure JDBC authentication for us. Added an SQL script to create a users table and an authorities table. Keep in mind that, within the Spring Security context, you have to follow the table name schema that Spring Security wants. Which is why users and authorities were picked as the names for my tables. I can customize this to be my own name, however, that would have meant being responsible for developing the code to access the data via JDBC code or JPA/Hibernate code like I have done before. Following Spring Security's pre-defined rules makes accessing Users and their Authroities a lot easier. The risk is, anyone who wants to mess up your database will know the names of these tables.

--- src/main/java/com/springboot/travelblog/travelblogapp/security/SecurityConfig.java
+++ src/main/java/com/springboot/travelblog/travelblogapp/security/SecurityConfig.java
@@ -8,11 +8,39 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/dataRest-api/travelBloggers").hasRole("BLOGGER")
+                        .requestMatchers(HttpMethod.GET, "/dataRest-api/travelBloggers/**").hasRole("BLOGGER")
+                        .requestMatchers(HttpMethod.POST, "/dataRest-api/travelBloggers").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/dataRest-api/travelBloggers/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/dataRest-api/travelBloggers/**").hasRole("ADMIN")
+        );
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
+    /*
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails john = User.builder()
@@ -35,22 +63,5 @@ public class SecurityConfig {
 
         return new InMemoryUserDetailsManager(john, mary, susan);
     }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(configurer ->
-                configurer
-                        .requestMatchers(HttpMethod.GET, "/dataRest-api/travelBloggers").hasRole("BLOGGER")
-                        .requestMatchers(HttpMethod.GET, "/dataRest-api/travelBloggers/**").hasRole("BLOGGER")
-                        .requestMatchers(HttpMethod.POST, "/dataRest-api/travelBloggers").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PUT, "/dataRest-api/travelBloggers/**").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.DELETE, "/dataRest-api/travelBloggers/**").hasRole("ADMIN")
-        );
-
-        http.httpBasic(Customizer.withDefaults());
-
-        http.csrf(csrf -> csrf.disable());
-
-        return http.build();
-    }
+     */
 }
