ca5435ee6635028878d3ba855b12ba617a6b2d9b
unknown <nvyas3095@gmail.com>
2023-08-15 16:56:28 -0700

    Changed table creation to have bcrypt passwords instead of plain text passwords. Password column needed 68 characters instead of 50 characters. Now, a User's password is stored in our db using the BCrypt encryption algorithm. Much more secure!

--- SQL Scripts/users and authorities tables creation.sql
+++ SQL Scripts/users and authorities tables creation.sql
@@ -9,7 +9,7 @@ DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` char(68) NOT NULL,
   `enabled` tinyint NOT NULL,
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -17,12 +17,18 @@ CREATE TABLE `users` (
 --
 -- Inserting data for table `users`
 --
+-- NOTE: The passwords are encrypted using BCrypt
+--
+-- A generation tool is avail at: https://www.luv2code.com/generate-bcrypt-password
+--
+-- Default passwords here are: fun123
+--
 
 INSERT INTO `users` 
 VALUES 
-('john','{noop}test123',1),
-('mary','{noop}test123',1),
-('susan','{noop}test123',1);
+('john','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1),
+('mary','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1),
+('susan','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1);
 
 
 --
@@ -32,8 +38,8 @@ VALUES
 CREATE TABLE `authorities` (
   `username` varchar(50) NOT NULL,
   `authority` varchar(50) NOT NULL,
-  UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
-  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+  UNIQUE KEY `authorities4_idx_1` (`username`,`authority`),
+  CONSTRAINT `authorities4_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -42,11 +48,9 @@ CREATE TABLE `authorities` (
 
 INSERT INTO `authorities` 
 VALUES 
-('john','ROLE_EMPLOYEE'),
-('mary','ROLE_EMPLOYEE'),
+('john','ROLE_BLOGGER'),
+('mary','ROLE_BLOGGER'),
 ('mary','ROLE_MANAGER'),
-('susan','ROLE_EMPLOYEE'),
+('susan','ROLE_BLOGGER'),
 ('susan','ROLE_MANAGER'),
 ('susan','ROLE_ADMIN');
-
-
