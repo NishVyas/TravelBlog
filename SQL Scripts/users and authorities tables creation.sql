2777badfc47b8ce9ff67d2e91636821bfe2ff40e
unknown <nvyas3095@gmail.com>
2023-08-15 22:45:11 -0700

    Configured Spring Security to use custom database table names instead of pre-defined ones. Modified SecurityConfig.java by setting a query for Spring Security to map their definition of a User to our members table and mapping their definition of Authorities to our roles table. Added DEBUG level logging for Spring Security beans. Updated the Postman collection.

--- SQL Scripts/users and authorities tables creation.sql
+++ SQL Scripts/users and authorities tables creation.sql
@@ -1,21 +1,21 @@
 USE `travelblogger_tracker`;
 
-DROP TABLE IF EXISTS `authorities`;
-DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `roles`;
+DROP TABLE IF EXISTS `members`;
 
 --
--- Table structure for table `users`
+-- Table structure for table `members`
 --
 
-CREATE TABLE `users` (
-  `username` varchar(50) NOT NULL,
-  `password` char(68) NOT NULL,
-  `enabled` tinyint NOT NULL,
-  PRIMARY KEY (`username`)
+CREATE TABLE `members` (
+  `user_id` varchar(50) NOT NULL,
+  `pw` char(68) NOT NULL,
+  `active` tinyint NOT NULL,
+  PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Inserting data for table `users`
+-- Inserting data for table `members`
 --
 -- NOTE: The passwords are encrypted using BCrypt
 --
@@ -24,8 +24,8 @@ CREATE TABLE `users` (
 -- Default passwords here are: fun123
 --
 
-INSERT INTO `users` 
-VALUES 
+INSERT INTO `members`
+VALUES
 ('john','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1),
 ('mary','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1),
 ('susan','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1);
@@ -35,19 +35,19 @@ VALUES
 -- Table structure for table `authorities`
 --
 
-CREATE TABLE `authorities` (
-  `username` varchar(50) NOT NULL,
-  `authority` varchar(50) NOT NULL,
-  UNIQUE KEY `authorities4_idx_1` (`username`,`authority`),
-  CONSTRAINT `authorities4_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+CREATE TABLE `roles` (
+  `user_id` varchar(50) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  UNIQUE KEY `authorities5_idx_1` (`user_id`,`role`),
+  CONSTRAINT `authorities5_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `members` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Inserting data for table `authorities`
+-- Inserting data for table `roles`
 --
 
-INSERT INTO `authorities` 
-VALUES 
+INSERT INTO `roles`
+VALUES
 ('john','ROLE_BLOGGER'),
 ('mary','ROLE_BLOGGER'),
 ('mary','ROLE_MANAGER'),
